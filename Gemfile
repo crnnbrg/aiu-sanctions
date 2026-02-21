b17d8b749a26db52d647a1a6e40c1b3f6141203f
dngst <dngst@protonmail.com>
2026-02-22 01:08:49 +0300

    Add scraped data

--- Gemfile
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-source 'https://rubygems.org'
-
-gem 'pdf-reader', '~> 2.15'
-
-gem 'csv', '~> 3.3'
-
-gem 'countries', '~> 8.0'
---
+++ data/get_country_name.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import country_converter as coco
+
+cc = coco.CountryConverter()
+df = pd.read_csv('aiu_ineligible_persons.csv')
+
+nationalities = df['Nationality'].tolist()
+df['Country Name'] = cc.convert(nationalities, to='name_short', src='IOC')
+df.to_csv('aiu_ineligible_persons.csv')
